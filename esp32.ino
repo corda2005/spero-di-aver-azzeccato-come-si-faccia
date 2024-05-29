18a4a21e0b8bdf3bd2ea75ed0e6e464bfef2fc2d
corda2005 <gabrielecorda02@gmail.com>
2024-05-30 01:39:05 +0200

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -1,5 +1,5 @@
-#define RX2 D4;
-#define TX2 D5; 
+#define RX2 D4
+#define TX2 D5
 
 void setup() {
   pinMode(D4, INPUT);
@@ -7,13 +7,13 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   
   Serial.begin(9600);
-  Serial.println("ricevo: ");
+  
   Serial2.begin(9600, SERIAL_8N1, RX2, TX2);
 }
 
 void loop() {
+  Serial.println("ricevo: ");
   if (Serial2.available()>0) { digitalWrite(LED_BUILTIN, HIGH);
-      Serial.write(Serial2.read());
+      Serial.println(Serial2.read());
   }
-  else digitalWrite(LED_BUILTIN, LOW);
 }
